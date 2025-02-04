b0f693814016e32cbaeeb356bd6bcf8f36dda1cf
Shivrajsinh-Jadeja <24010101106@darshan.ac.in>
2025-02-04 23:05:38 +0530

    Add files via upload
    made classes

--- Javaproject.java
+++ Javaproject.java
@@ -1,5 +1,32 @@
+class Train{
+    int trainNum;
+    int totalSeatsInCoach;
+    String trainName;
+}
+
+class Passenger{
+    String passengerName;
+    String email;
+    char Gender;
+    long mobile_Number;
+}
+
+class Destination{
+    String startingStation;
+    String endingStation;
+    int totalDistanceInKilometres;
+}
+
+class TicketDetails{
+    int totalDistance;
+    float totalFareForCoach;
+    float totalSumOfWholeTrip;
+    long pnrNum;
+}
+
+
 public class Javaproject {
     public static void main(String[] args){
-
-    }   
+             
+    }    
 }
